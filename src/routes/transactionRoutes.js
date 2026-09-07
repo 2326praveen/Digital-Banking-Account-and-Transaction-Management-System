edf82be386074b7322bf41b581efcfc991444244
Bennett-13 <bennett.roy@btech.christuniversity.in>
2026-09-07 14:05:09 +0530

    Update transactionRoutes.js

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,15 +1,8 @@
+// routes/transactionRoutes.js
 const express = require("express");
 const router = express.Router();
-const {
-  getHistory,
-  getStatement,
-  getFlagged,
-  runInterest,
-} = require("../controllers/transactionController");
+const { getTransactions } = require("../controllers/transactionController");
 
-router.get("/:id/transactions", getHistory);
-router.get("/:id/statement", getStatement);
-router.get("/flagged", getFlagged);
-router.post("/interest/run", runInterest);
+router.get("/:accountId/transactions", getTransactions);
 
 module.exports = router;
